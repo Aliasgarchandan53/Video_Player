65eb592f04d8966f00f304b08ff8446aa8d03d37
Aliasgarchandan53 <aliasgarchandan53@gmail.com>
2025-02-09 11:05:53 +0530

    Added utilities and custom classes for errors and responses

---
+++ Backend/src/app.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+const app = express();
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+//use to allow express to share json data across server
+app.use(express.json({limit:"16kb"}))
+
+//used to specofy url encodings in express , eg:specify if url includes objects etc.
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16kb"
+}))
+
+//informs express about the location of static resources like images
+app.use(express.static("public"))
+
+//allows devs to use the cookies and insert secure cookies in request
+app.use(cookieParser())
+
+export { app }
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,6 +5,17 @@ dotenv.config({
     path:'./env'
 })
 connectDB()
+.then(()=>{
+    app.on("error",(err)=>{
+        console.log("DB server error : ",err)
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Sever is running at port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection error : ",err)
+})
 
 /*
 //approach 1
