002c7a2819507b6c34caf7cc2c4fd3e1ca5b6f63
Aliasgarchandan53 <aliasgarchandan53@gmail.com>
2025-02-20 11:17:00 +0530

    edited user routes

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,36 +1,51 @@
-import {  Router } from "express"
-import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js"
-import {upload} from "../middlewares/multer.middleware.js"
-import { verifyJWT } from "../middlewares/auth.middleware.js"
-const router = Router()
+import { Router } from "express";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name:"avatar", //keep same name in frontend
-            maxCount:1
-        },{
-            name:"coverImage",
-            maxCount:1
-        }
-    ]),
-    registerUser
-)
+  upload.fields([
+    {
+      name: "avatar", //keep same name in frontend
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post( verifyJWT ,logoutUser)
-router.route("/refreshToken").post( refreshAccessToken)//verifyJwt is not exactly required
-router.route("/changeCurrentPassword").post(verifyJWT,changeCurrentPassword)
-router.route("/getCurrentUser").get(verifyJWT,getCurrentUser)
-router.route("/updateAccountDetails").post(verifyJWT,updateAccountDetails)
-router.route("/updateAvatar").post(
-    upload.single("avatar"),
-    updateUserAvatar
-)
-router.route("/updateCoverImage").post(
-    upload.single("coverImage"),
-    updateUserCoverImage
-)
-export default router
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken); //verifyJwt is not exactly required
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-details").post(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .post(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .post(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
+
+export default router;
