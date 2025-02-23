5d697ce3f0a6cb68352de047c4f0db35c10fa04c
Aliasgarchandan53 <aliasgarchandan53@gmail.com>
2025-02-23 20:30:47 +0530

    Added subscription controller-subscribe function

--- Backend/src/controllers/subscription.controller.js
+++ Backend/src/controllers/subscription.controller.js
@@ -8,6 +8,38 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
   // TODO: toggle subscription
+  //get channel id from params 
+  //if id exists, create  a subscriber document.
+  //get channel-user via channel id 
+  //subscriber- user, channel- channel via channel id
+  //if susccessfully created, send a response that channel is subscribed
+  if(!channelId){
+    throw new ApiError(400,"Channel id is requuired.")
+  }
+  channelId = isValidObjectId(channelId)? new mongoose.Types.ObjectId(channelId):null;
+  const channel = await User.findById(channelId);
+  if(!channel){
+    throw new ApiError(500,"Channel does not exist.");
+  }
+  const user = await User.findById(req.user?._id);
+  if(!user){
+    throw new ApiError(501,"Unable to fetch current user");
+  }
+
+  const subscription = await Subscription.create(
+    {
+      subscriber:user,
+      channel:channel
+    }
+  )
+  if(!subscription){
+    throw new ApiError(503,"Something went wrong while subscribing the channel".)
+  }
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,subscription,"Channel was subscribed successfully.")
+  )
 });
 
 // controller to return subscriber list of a channel
